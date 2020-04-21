54b8c1b1ce32d219a327926fd45c779fe1207ee0
Martin Beneš <martinbenes1996@gmail.com>
2020-04-21 16:36:14 +0200

    setup scripts

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from distutils.core import setup
 setup(
   name = 'slovakrailways',
-  packages = ['slovakrailways'],
+  packages = ['.'],
   version = '0.0.1',
   license='MIT',
   description = 'Python envelope of Slovak Railways API',
   author = 'Martin Beneš',
   author_email = 'martinbenes1996@gmail.com',
   url = 'https://github.com/martinbenes1996/slovakrailways',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/martinbenes1996/slovakrailways/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/martinbenes1996/slovakrailways/archive/v0.0.1.tar.gz',    # I explain this later on
   keywords = ['API', 'Railway', 'Train', 'Slovakia', 'Data', 'REST API'],
   install_requires=[],
+  package_dir={'': '.'},
   classifiers=[
     'Development Status :: 3 - Alpha',
     'Intended Audience :: Developers',
